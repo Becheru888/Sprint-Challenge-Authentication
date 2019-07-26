239e2f96ac96e1b61bb7be9cca294dd65ac5dc33
Becheru888 <becheru93@gmail.com>
2019-07-26 08:52:19 +0100

    Token generator created

---
+++ auth/token.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.export ={
+    generateToken
+};
+
+
+function generateToken(user){
+    const payload ={
+        subject: user.id,
+        username: user.username,
+        roles:['user']
+    };
+    const options = {
+        expiresIn = "1d"
+    };
+    return jwt.sign(payload, process.env.JWT_SECRET, options)
+}
